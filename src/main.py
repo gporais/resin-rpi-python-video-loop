8a3c6ed859eae68f4de5581ecfc24ea87ecdc320
George Phillip Orais <gporais@yahoo.com>
2017-10-04 17:30:30 +0900

    Testing 1

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-# from omxplayer.player import OMXPlayer
-# from pathlib import Path
+from omxplayer.player import OMXPlayer
+#from pathlib import Path
 # from time import sleep
 
 # VIDEO_PATH = Path("./media/test1.mp4")
 
 #player = OMXPlayer(VIDEO_PATH)
-# vid = OMXPlayer('./media/test1.mp4',args=['--loop'])
-
+player = OMXPlayer('../media/test1.mp4', args=['--loop'])
 #vid.play()
 
 # sleep(5)
@@ -22,6 +21,6 @@
 
 # movie_path = '../media/test2.mp4'
 
-from subprocess import Popen
+# from subprocess import Popen
 
-omxp = Popen(['omxplayer', '-o', 'local', '--loop', '--no-osd' , '../media/test2.mp4'])
+# omxp = Popen(['omxplayer', '-o', 'local', '--loop', '--no-osd' , '../media/test2.mp4'])
